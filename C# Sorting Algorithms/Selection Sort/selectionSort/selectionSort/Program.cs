348746f6cec5ab8ae449dc823ddfb82393145abe
Umut <umuty897@gmail.com>
2024-10-15 01:51:59 +0300

    Add files via upload

---
+++ C# Sorting Algorithms/Selection Sort/selectionSort/selectionSort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 21, 35, 12, 41, 13, 72, 52, 61, 11, 48 };
+
+            int temp;
+            int minIndex;
+            
+            Console.WriteLine("** Before the sorting starts **\n");
+
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n----------------------------------------\n");
+
+            //Sorting algorithms
+            for(int i = 0; i < array.Length - 1; i++) 
+            {
+                minIndex = i;
+
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                        
+                    }
+                }
+                temp = array[i];
+                array[i] =array[minIndex];
+                array[minIndex] = temp;
+
+                
+                foreach (int k in array)
+                {
+
+                    Console.Write(k + " ");
+                }
+                Console.WriteLine($" | Step {i+1}");
+
+            }
+
+            Console.WriteLine("\n----------------------------------------");
+            Console.WriteLine("** After the sorting is over** \n");
+
+            foreach (int k in array)
+            {
+                Console.Write(k + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ C# Sorting Algorithms/Selection Sort/selectionSort/selectionSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("selectionSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("selectionSort")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e68c59a-143d-4442-b190-497c67e86bfb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
